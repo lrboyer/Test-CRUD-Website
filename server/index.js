2f9548a4f610f2f10c78fc5322def826eae3f29f
lrboyer <lrboyer@clemson.edu>
2022-02-26 13:36:11 -0500

    First iteration many problems

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password", 
+    database: "cruddatabase",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res) => {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlSelect = "SELECT * FROM movie_reviews";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+});
+
+
+app.post("/api/insert", (req, res) => {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?, ?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(result);
+    })
+});
+
+app.delete("/api/delete/:movieName", (req, res) => {
+    const name = req.params.movieName;
+    const sqlDelete = "DELETE FROM movie_reviews WHERE movieName = ?";
+    
+    db.query(sqlDelete, name, (err, result) => {
+        if (err) console.log(err);
+    });
+});
+
+app.put("/api/update/", (req, res) => {
+    const name = req.body.movieName;
+    const review = req.body.movieReview;
+    const sqlUpdate = "UPDATE movie_reviews  movieReview = ? WHERE movieName = ?";
+    
+    db.query(sqlUpdate, [review, name], (err, result) => {
+        if (err) console.log(err);
+    });
+});
+
+
+app.listen(3002, () => {
+    console.log("running on port 3002");
+});
